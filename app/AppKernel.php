e13b97f14db0c9e38aa0b84ef73a951a02afeb85
Administrator <alexandre.lippmann@free.fr>
2017-10-06 10:35:35 +0200

    [Tech] Application du thème du site

--- app/AppKernel.php
+++ app/AppKernel.php
@@ -16,6 +16,7 @@ class AppKernel extends Kernel
             new Doctrine\Bundle\DoctrineBundle\DoctrineBundle(),
             new Sensio\Bundle\FrameworkExtraBundle\SensioFrameworkExtraBundle(),
             new Symfony\Bundle\AsseticBundle\AsseticBundle(),
+            new Gosyl\CommonBundle\GosylCommonBundle(),
             new Gosyl\DataTableBundle\GosylDataTableBundle(),
             new Gosyl\MyCarbuConsoBundle\GosylMyCarbuConsoBundle()
         ];
